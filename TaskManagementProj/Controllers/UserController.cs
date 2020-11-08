c66ab4d4f9b91588aba03bc612544b0bf6f7cd8e
XiaochenLiu <Jmoriartybw007@gmail.com>
2020-11-08 00:13:08 -0600

    Recreate UserController to replace RoleController to avoid some errors

---
+++ TaskManagementProj/Controllers/UserController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TaskManagementProj.Models;
+
+namespace TaskManagementProj.Controllers
+{
+    //[Authorize]
+    //[Authorize(Roles = "Admin")]
+    public class UserController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetAllUserNames()
+        {
+            var users = UserManager.GetAllUserNames();
+            return View(users);
+        }
+
+        public ActionResult GetAllRoles()
+        {
+            var roles = UserManager.GetAllRoles();
+            return View(roles);
+        }
+
+        public ActionResult AddNewRole()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddNewRole(string roleName)
+        {
+            UserManager.AddNewRole(roleName);
+            db.SaveChanges();
+            db.Dispose();
+            return RedirectToAction("GetAllRoles");
+        }
+
+        public ActionResult AddUserToRole(string userId)
+        {
+            ViewBag.UserName = db.Users.Find(userId).UserName;
+            ViewBag.roleName = new SelectList(db.Roles, "Name", "Name");
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult AddUserToRole(string roleName, string userId)
+        {
+            var r = UserManager.AddUserToRole(userId, roleName);
+            if (r.Succeeded)
+            {
+                db.SaveChanges();
+            }
+
+            ViewBag.UserName = db.Users.Find(userId).UserName;
+            ViewBag.roleName = new SelectList(db.Roles, "Name", "Name");
+            db.Dispose();
+            return RedirectToAction("ShowAllRolesOfTheUser", new { userId });
+        }
+    }
+}
