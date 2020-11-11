7d425d82554e39656d8793dc1ec2b86358326fb3
Rui Ma <mrsckd3035@gmail.com>
2020-11-11 11:06:46 -0600

    Fixed Bug

---
+++ TaskManagementProj/Migrations/202011102124108_MergedCode.cs
@@ -0,0 +1,18 @@
+﻿namespace TaskManagementProj.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MergedCode : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Notifications", "CreateTime", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Notifications", "CreateTime");
+        }
+    }
+}
